030b26fa956c9485c0dcd8ac9cfbcf123002b9dd
HeavenVolkoff <vasvas10@gmail.com>
2017-07-18 16:36:22 -0300

    Implement a better command line argument parsing using getopt_long(...) Fixed sizeof bug in filter message's length calculation A small improvement to the help message Changed filter's argument to allow more options

--- maclog.m
+++ maclog.m
@@ -143,11 +143,75 @@ void prepareLogArgv(int type) {
     }
 }
 
+void printHelpAndExit (int exitStatus) {
+    printf(
+            "USAGE: maclog [--version|-v] [--help|-h]\n"
+            "USAGE: maclog [logMode] [-f|--filter=<query>]\n"
+            "Arguments:\n"
+            " --help,     -h  Show maclog help info.\n"
+            " --version,  -v  Show maclog version info.\n"
+            "Log Modes:\n"
+            " --boot,     -b  Show log messages since last boot time.\n"
+            " --darkWake, -d  Show log messages since last darkWake time.\n"
+            " --sleep,    -s  Show log messages since last sleep time.\n"
+            " --stream,   -S  Show log messages in real time.\n"
+            " --wake,     -w  Show log messages since last wake time.\n"
+            "Filter:\n"
+            " --filter,   -f  Show log messages filtered by the <query>.\n"
+            "NOTE: The default behaviour is to show all log messages of the current day.\n"
+            "NOTE: The messages returned by \e[1m--boot\e[0m, \e[1m--sleep\e[0m, "
+            "\e[1m--wake\e[0m, \e[1m--darkWake\e[0m can be from previous days,"
+            " depending on the last time each action occurred.\n"
+            "NOTE: The \e[1m--stream\e[0m option results in the maclog process being never finished,"
+            " due to the necessity of redirecting all logs to Console in real-time.\n"
+    );
+    exit(exitStatus);
+}
+
 int main(int argc, char **argv)
 {
+    int mode = 0;
+    int filterFlag = 0;
+    int optionIndex = 0;
+    int currentOption = getopt_long (argc, argv, shortOptions, longOptions, &optionIndex);
+
+    //
+    // Handle arguments
+    //
+    while (currentOption != -1)
+    {
+        switch (currentOption) {
+            case 'f':
+                if (filterFlag) {
+                    printf("ERROR: Filter argument should only appear once.\n");
+                    exit(EXIT_FAILURE);
+                }
+
+                filterFlag = 1;
+                gLogArgs[gLogFilter] = calloc(sizeof(predicate filterConcat) + strlen(optarg) + 1, sizeof(char));
+                sprintf(gLogArgs[gLogFilter], predicate filterConcat "%s", optarg);
+                break;
+            case 'h':
+                printHelpAndExit(EXIT_SUCCESS);
+            case 'v':
+                printf("v%.1f (c) 2017 syscl/lighting/Yating Zhou\n", PROGRAM_VER);
+                exit(EXIT_SUCCESS);
+            case '?':
+                printHelpAndExit(EXIT_FAILURE);
+            default:
+                if (mode) {
+                    printf("ERROR: Different modes can't be mixed.\n");
+                    printHelpAndExit(EXIT_FAILURE);
+                }
+
+                mode = currentOption;
+                break;
+        }
+
+        currentOption = getopt_long (argc, argv, shortOptions, longOptions, &optionIndex);
+    }
+
     pid_t rc;
-    char default_str[] = "(process == \"kernel\" OR eventMessage CONTAINS[c] \"kernel\")";
-    char filter_str[]  = " AND eventMessage CONTAINS[c] \"";
     if ((rc = fork()) > 0)
     {
         //
@@ -158,104 +222,44 @@ int main(int argc, char **argv)
         // create the log file
         //
         int fd = open(gLogPath, O_CREAT | O_TRUNC | O_RDWR, PERMS);
-        
-        //
-        // Handle arguments
-        //
-        
-        //
-        // get filter
-        //
-        switch (argc) {
-            case 3:
-                if (strcmp(argv[1], "--filter") == 0 || strcmp(argv[1], "-f") == 0) {
-                    gLogArgs[gLogFilter] = (char *)malloc(sizeof(char) * 2 + sizeof(default_str) + sizeof(filter_str) + sizeof(argv[2]));
-                    sprintf(gLogArgs[gLogFilter], "%s%s%s\"", default_str, filter_str, argv[2]);
-                }
+
+        switch (mode) {
+            case 0:
+                prepareLogArgv(showLogArgv);
+                gLogArgs[gLogTime] = gCurTime();
                 break;
-            case 4:
-                if (strcmp(argv[2], "--filter") == 0 || strcmp(argv[2], "-f") == 0) {
-                    gLogArgs[gLogFilter] = (char *)malloc(sizeof(char) * 2 + sizeof(default_str) + sizeof(filter_str) + sizeof(argv[3]));
-                    sprintf(gLogArgs[gLogFilter], "%s%s%s\"", default_str, filter_str, argv[3]);
-                }
+            case 'b':
+                prepareLogArgv(showLogArgv);
+                gLogArgs[gLogTime] = gBootTime();
+                break;
+            case 'd':
+                prepareLogArgv(showLogArgv);
+                gLogArgs[gLogTime] = gPowerManagerDomainTime(kPMASLDomainPMDarkWake);
+                break;
+            case 's':
+                prepareLogArgv(showLogArgv);
+                gLogArgs[gLogTime] = gPowerManagerDomainTime(kPMASLDomainPMSleep);
+                break;
+            case 'S':
+                prepareLogArgv(streamLogArgv);
+                break;
+            case 'w':
+                prepareLogArgv(showLogArgv);
+                gLogArgs[gLogTime] = gPowerManagerDomainTime(kPMASLDomainPMWake);
                 break;
             default:
-                gLogArgs[gLogFilter] = default_str;
-                break;
+                if(access(gLogPath, F_OK) == 0) unlink(gLogPath);
+                printf("ERROR: Invalid mode: %c\n", mode);
+                printHelpAndExit(EXIT_FAILURE);
         }
-        //
-        // get option
-        //
-        if (argc > 1)
-        {
-            if (strcmp(argv[1], "--stream") == 0 || strcmp(argv[1], "-S") == 0) {
-                prepareLogArgv(streamLogArgv);
-            } else {
-                prepareLogArgv(showLogArgv);
-                if (strcmp(argv[1], "--boot") == 0 || strcmp(argv[1], "-b") == 0)
-                {
-                    gLogArgs[gLogTime] = gBootTime();
-                }
-                else if (strcmp(argv[1], "--sleep") == 0 || strcmp(argv[1], "-s") == 0)
-                {
-                    gLogArgs[gLogTime] = gPowerManagerDomainTime(kPMASLDomainPMSleep);
-                    
-//                    for(int i=0; i<11; i++) printf("%s, ", gLogArgs[i]);
-//                    return EXIT_SUCCESS;
-                }
-                else if (strcmp(argv[1], "--wake") == 0 || strcmp(argv[1], "-w") == 0)
-                {
-                    gLogArgs[gLogTime] = gPowerManagerDomainTime(kPMASLDomainPMWake);
-                }
-                else if (strcmp(argv[1], "--darkWake") == 0 || strcmp(argv[1], "-d") == 0)
-                {
-                    gLogArgs[gLogTime] = gPowerManagerDomainTime(kPMASLDomainPMDarkWake);
-                }
-                else if(strcmp(argv[1], "--version") == 0 || strcmp(argv[1], "-v") == 0){
-                    if(access(gLogPath, F_OK) == 0) unlink(gLogPath);
-                    printf("v%.1f (c) 2017 syscl/lighting/Yating Zhou\n", PROGRAM_VER);
-                    exit(EXIT_SUCCESS);
-                }
-                else
-                {
-                    // TODO: need more improvement, sorry for being so lazy!
-                    if(argc != 3 && argc != 4){
-                        if(access(gLogPath, F_OK) == 0) unlink(gLogPath);
-                        printf(
-                               "Invalid argument(s).\n"
-                               "USAGE: maclog [option] [-f|--filter <query>]\n"
-                               " --boot, -b     Show log messages since last boot time.\n"
-                               " --darkWake, -d Show log messages since last darkWake time.\n"
-                               " --filter, -f   Show log messages filtered by the <query>.\n"
-                               " --stream, -S   Show log messages in real time.\n"
-                               " --sleep, -s    Show log messages since last sleep time.\n"
-                               " --version, -v  Show this maclog version info.\n\n"
-                               " --wake, -w     Show log messages since last wake time.\n"
-                               "NOTE: The default behaviour is to show all log messages of the current day.\n"
-                               "NOTE: The messages returned by \e[1m--boot\e[0m, \e[1m--sleep\e[0m, "
-                               "\e[1m--wake\e[0m, \e[1m--darkWake\e[0m can be from previous days,"
-                               " depending on the last time each action occurred.\n"
-                               "NOTE: The \e[1m--stream\e[0m option results in the maclog process being never finished,"
-                               " due to the necessity of redirecting all logs to Console in real-time.\n"
-                               );
-                        exit(EXIT_FAILURE);
-                    }
-                    gLogArgs[gLogTime] = gCurTime();
-                }
-            }
-        }
-        else
-        {
-            prepareLogArgv(showLogArgv);
-            gLogArgs[gLogTime] = gCurTime();
-        }
+
         //
         // log the log file
         //
         if (fd >= 0)
         {
             if (dup2(fd, STDOUT_FILENO) < 0) {
-                printf("Failed to retrieve logs.\n");
+                printf("ERROR: Failed to retrieve logs.\n");
                 exit(EXIT_FAILURE);
             }
         }
@@ -278,7 +282,7 @@ int main(int argc, char **argv)
         //
         // fork failed
         //
-        printf("Fork failed\n");
+        printf("ERROR: Fork failed\n");
         return EXIT_FAILURE;
     }
 
